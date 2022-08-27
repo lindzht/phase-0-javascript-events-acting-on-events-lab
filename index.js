42f11b4d0b118138d896a49f1054a9e2aa8d974f
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-08-27 13:29:58 -0400

    commit tree 09b0590151a3f830533a1f6eddd3c56d70baed1b

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.left = "0px";
+// dodger.style.bottom = "0px";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  };
+
+function moveDodgerRight (){
+    const currentPosition = dodger.style.left.replace("px", "");
+    const moveAwayFromLeft = parseInt(currentPosition, 10);
+    if (moveAwayFromLeft < 360){
+        dodger.style.left = `${moveAwayFromLeft + 1}px`;
+    }
+};
+
+document.addEventListener("keydown", function(e){
+    if (e.key === "ArrowLeft"){
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
+
+
